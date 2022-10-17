c5b4aadb85b5b3523333682e6b3f2b4b152d890f
Roman Gulevich <r.gulevich@syberry.com>
2022-10-17 13:32:45 +0300

    Added implementation.

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,30 @@
 package com.epam.mjc.nio;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Map<String, String> map = new HashMap<>();
+        try (BufferedReader reader = new BufferedReader(new java.io.FileReader(file));) {
+            List<String> list = Files.readAllLines(file.toPath());
+            for (String stringOfFile : list) {
+                String[] split = stringOfFile.split("[:|\\n]");
+                map.put(split[0].trim(), split[1].trim());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return new Profile(map.get("Name"), Integer.parseInt(map.get("Age")), map.get("Email"), Long.parseLong(map.get("Phone")));
     }
 }
